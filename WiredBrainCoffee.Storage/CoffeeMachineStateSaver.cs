8437f2b003f9fda9fdc35431d413441add5d4eb1
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2020-06-02 17:21:44 -0500

    Migrating .NET Framework Libraries to .NET Standard

--- WiredBrainCoffee.Storage/CoffeeMachineStateSaver.cs
+++ WiredBrainCoffee.Storage/CoffeeMachineStateSaver.cs
@@ -1,45 +1,47 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using System.IO;
 
 namespace WiredBrainCoffee.Storage
 {
-  public class CoffeeMachineStateSaver
-  {
-    private readonly string _filename = "machine.json";
-
-    public void Save(CoffeeMachineState state)
+    public class CoffeeMachineStateSaver
     {
-      var json = JsonConvert.SerializeObject(state);
-      File.WriteAllText(_filename, json);
-    }
+        private readonly string _filename = "machine.json";
 
-    public CoffeeMachineState Load()
-    {
-      if (File.Exists(_filename))
-      {
-        var json = File.ReadAllText(_filename);
-        return JsonConvert.DeserializeObject<CoffeeMachineState>(json);
-      }
+        public void Save(CoffeeMachineState state)
+        {
+            var json = JsonConvert.SerializeObject(state);
+            File.WriteAllText(_filename, json);
+        }
 
-      return new CoffeeMachineState();
-    }
+        public CoffeeMachineState Load()
+        {
+            if (File.Exists(_filename))
+            {
+                var json = File.ReadAllText(_filename);
+                return JsonConvert.DeserializeObject<CoffeeMachineState>(json);
+            }
 
-    public void ShowStoredJson()
-    {
-      var json = File.Exists(_filename) ? File.ReadAllText(_filename) : "<empty>";
-      var window = new System.Windows.Window
-      {
-        Title = "Stored JSON",
-        Content = json,
-        WindowStyle = System.Windows.WindowStyle.ToolWindow,
-        WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen,
-        FontSize = 20,
-        Padding = new System.Windows.Thickness(5),
-        Width = 300,
-        Height = 300
-      };
-      window.Content = json;
-      window.Show();
+            return new CoffeeMachineState();
+        }
+
+        public void ShowStoredJson()
+        {
+            var json = File.Exists(_filename) ? File.ReadAllText(_filename) : "<empty>";
+            Console.WriteLine($"Stored JSON: {json}");
+            //var window = new System.Windows.Window
+            //{
+            //  Title = "Stored JSON",
+            //  Content = json,
+            //  WindowStyle = System.Windows.WindowStyle.ToolWindow,
+            //  WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen,
+            //  FontSize = 20,
+            //  Padding = new System.Windows.Thickness(5),
+            //  Width = 300,
+            //  Height = 300
+            //};
+            //window.Content = json;
+            //window.Show();
+        }
     }
-  }
 }
